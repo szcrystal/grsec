86cf5879293d1b90fac704775416e999a722793b
“szcrystal” <szk.create@gmail.com>
2018-04-28 04:37:34 +0000

    gr

--- resources/views/dashboard/item/index.blade.php
+++ resources/views/dashboard/item/index.blade.php
@@ -54,6 +54,7 @@
         </div>
         -->
 
+
         <div class="card-body">
           <div class="table-responsive">
             <table class="table table-striped table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
