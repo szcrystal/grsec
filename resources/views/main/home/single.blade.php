5c5c75b3c8c2f1a23f294b6cfbf291b5b6b4c726
“szcrystal” <szk.create@gmail.com>
2018-05-01 03:13:01 +0000

    gr

--- resources/views/main/home/single.blade.php
+++ resources/views/main/home/single.blade.php
@@ -127,11 +127,11 @@ use App\User;
                     
                     <div class="mt-4">
                     	{{ $item->title}} とは
-                    	<p>{{ $item->what_is }}</p>
+                    	<p>{{ nl2br($item->what_is) }}</p>
                     </div>
                     
                     <div class="mt-4">
-                    	{{ $item->warning }}
+                    	{{ nl2br($item->warning) }}
                     </div>
 					
 
