b6612d3d8e96cd272ad35b73b7dec696042bb5e1
“szcrystal” <szk.create@gmail.com>
2019-01-09 14:29:00 +0900

    grbeta

--- database/migrations/2018_12_27_145845_add_column_to_etc.php
+++ database/migrations/2018_12_27_145845_add_column_to_etc.php
@@ -18,7 +18,13 @@ class AddColumnToEtc extends Migration
         	$table->integer('permission')->after('email')->nullable()->default(NULL);
         });
         
+        Schema::table('sale_relations', function (Blueprint $table) {
+        	$table->text('information_foot')->after('information')->nullable()->default(NULL);
+        });
         
+        Schema::table('send_mail_flags', function (Blueprint $table) {
+        	$table->text('information_foot')->after('information')->nullable()->default(NULL);
+        });
     }
 
     /**
@@ -33,5 +39,18 @@ class AddColumnToEtc extends Migration
                 $table->dropColumn('permission');
             });
         }
+        
+        if (Schema::hasColumn('sale_relations', 'information_foot')) {
+            Schema::table('sale_relations', function (Blueprint $table) {
+                $table->dropColumn('information_foot');
+            });
+        }
+        
+        if (Schema::hasColumn('send_mail_flags', 'information_foot')) {
+            Schema::table('send_mail_flags', function (Blueprint $table) {
+                $table->dropColumn('information_foot');
+            });
+        }
+        
     }
 }
