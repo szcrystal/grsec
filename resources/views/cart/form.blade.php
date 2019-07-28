2191971c7398980b3d679f453bdf94fbce4dac47
“szcrystal” <szk.create@gmail.com>
2019-07-28 15:00:15 +0900

    grbeta

--- resources/views/cart/form.blade.php
+++ resources/views/cart/form.blade.php
@@ -995,8 +995,8 @@ use App\DeliveryGroup;
                                     {{--
                                     <input type="radio" name="pay_method" class="payMethodRadio ml-2" value="{{ $method->id }}" disabled> {{ $method->name }}
                                     --}}
-                                    <span class="text-secondary text-small ml-0"><i class="fas fa-exclamation-circle"></i> ご注文商品の代金引換はご利用できません。</span> 
-                                 
+                                    
+                                    <span class="text-secondary text-small ml-0"><i class="fas fa-exclamation-circle"></i> ご注文商品の代金引換不可</span>
                                     
                                 @else
                                 	<input id="radio-pay-{{ $method->id }}" type="radio" name="pay_method" value="{{ $method->id }}" class="payMethodRadio"{{ $checked }}>
